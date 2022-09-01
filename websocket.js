85f2bd6253bd0deee7860b9a0117bd9a707c8509
OlekVano <olekvano@gmail.com>
2022-09-02 00:50:08 +0100

    Changed the structure of handleClientConnection

--- websocket.js
+++ websocket.js
@@ -14,53 +14,59 @@ const handle_wss = (wss) => {
 const handleClientConnection = (client) => {
 	console.log('A new client Connected!')
 
-	handleMessage(client)
+	client.on('message', async (msg) => await handleMessage(msg, client))
+	client.on('close', async () => await handleClose(client))
 }
 
-const handleMessage = (client) => {
-	client.on('message', async (msg) => {
-		try {
-			const json = JSON.parse(msg)
-			if ('rooms' in json) {
-				console.log('Join')
-				for (var i = 0; i < json.rooms.length; i++) {
-					if (await validateRoom(json.rooms[i])) {
-						const id = json.rooms[i].id
-						try {
-							rooms[id].clients.push(client)
-						}
-						catch {
-							rooms[id] = {}
-							rooms[id].clients = [client]
-						}
+const handleMessage = async (msg, client) => {
+	try {
+		const json = JSON.parse(msg)
+		if ('rooms' in json) {
+			console.log('Join')
+			for (var i = 0; i < json.rooms.length; i++) {
+				if (await validateRoom(json.rooms[i])) {
+					console.log('Valid rooms')
+					const id = json.rooms[i].id
+					try {
+						rooms[id].clients.push(client)
 					}
-				}
-			}
-			if ('message' in json) {
-				console.log('Message')
-				if (await validateRoom(json)) {
-					for (var i = 0; i < rooms[json.id].clients.length; i++) {
-						console.log('send')
-						rooms[json.id].clients[i].send(JSON.stringify({
-							from: json.from,
-							message: json.message,
-							id: json.id
-						}))
+					catch {
+						rooms[id] = {}
+						rooms[id].clients = [client]
 					}
 				}
 			}
 		}
-		catch (err) {
-			console.log(`Error: ${err}`)
-		}
-		
-		/*if (message.split(' ')[0] === 'Access')
-		wss.clients.forEach(function each(client) {
-			if (client !== ws && client.readyState === WebSocket.OPEN) {
-				client.send(message)
+		if ('message' in json) {
+			console.log('Message')
+			if (await validateRoom(json)) {
+				for (var i = 0; i < rooms[json.id].clients.length; i++) {
+					console.log('send')
+					rooms[json.id].clients[i].send(JSON.stringify({
+						from: json.from,
+						message: json.message,
+						id: json.id
+					}))
+				}
 			}
-		});*/
-	});
+		}
+	}
+	catch (err) {
+		console.log(`Error: ${err}`)
+	}
+}
+
+const handleClose = async (client) => {
+	console.log('Close')
+	for (var key in rooms) {
+		//console.log(key)
+		//console.log(client)
+		//console.log(rooms[key].clients)
+		if (client in rooms[key].clients) {
+			//console.log('IN')
+			rooms[key].clients = rooms[key].clients.filter((i) => i !== client)
+		}
+	}
 }
 
 module.exports = handle_wss
