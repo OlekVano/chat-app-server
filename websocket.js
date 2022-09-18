99a5a58f936a576209445003d362a180f3a05a0d
OlekVano <olekvano@gmail.com>
2022-09-18 20:02:35 +0100

    Changed the websocket connection logic

--- websocket.js
+++ websocket.js
@@ -21,36 +21,37 @@ const onClientConnection = (client) => {
 const handleMessage = async (msg, client) => {
   try {
     const json = JSON.parse(msg)
-    if ('rooms' in json) {
-      console.log('Join')
-      for (var i = 0; i < json.rooms.length; i++) {
-        if (await validateRoom(json.rooms[i])) {
-          console.log('Valid rooms')
-          const id = json.rooms[i].id
-          try {
-            rooms[id].clients.push(client)
-          }
-          catch {
-            rooms[id] = {}
-            rooms[id].clients = [client]
-          }
-        }
-      }
-
-      console.log(rooms)
+    if (!'action' in json) {
+      console.log('No action key');
+      return
     }
-    if ('message' in json) {
-      console.log('Message')
-      if (await validateRoom(json)) {
-        for (var i = 0; i < rooms[json.id].clients.length; i++) {
-          console.log('send')
-          rooms[json.id].clients[i].send(JSON.stringify({
-            from: json.from,
-            message: json.message,
-            id: json.id
-          }))
+    if (json.action === 'roomsJoin') {
+      console.log('Join')
+      const roomsToJoin = json.rooms.filter(async (room) => await validateRoom(room))
+      roomsToJoin.forEach(room => {
+        const id = room.id
+        if (id in rooms) {
+          rooms[id].clients.push(client)
         }
-      }
+        else {
+          rooms[id] = {clients: [client]}
+        }
+      })
+    }
+    else if (json.action === 'messageSend') {
+      console.log('Message')
+      if (!await validateRoom(json)) return
+      rooms[json.id].clients.forEach(client => {
+        console.log('send')
+        client.send(JSON.stringify({
+          from: json.from,
+          message: json.message,
+          id: json.id
+        }))
+      })
+    }
+    else {
+      console.log(`Invalid action: ${json.action}`)
     }
   }
   catch (err) {
@@ -59,31 +60,10 @@ const handleMessage = async (msg, client) => {
 }
 
 const handleClose = async (client) => {
-  /*console.log('Close')
-  console.log(client)
-  console.log(rooms)
-  for (var key in rooms) {
-    //console.log(key)
-    //console.log(client)
-    //console.log(rooms[key].clients)
-    if (client in rooms[key].clients) {
-      console.log('IS')
-      //console.log('IN')
-      rooms[key].clients = rooms[key].clients.filter((i) => i !== client)
-    }
-  }
-  console.log(rooms)*/
-
-  console.log('Close')
-  //console.log(client)
-  //console.log(rooms)
   for (var key in rooms) {
     console.log(key)
-    //console.log(client)
-    //console.log(rooms[key].clients)
     if (rooms[key].clients.includes(client)) {
       console.log('IS')
-      //console.log('IN')
       rooms[key].clients = rooms[key].clients.filter((i) => i !== client)
     }
   }
